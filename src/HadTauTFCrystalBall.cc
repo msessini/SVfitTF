7c49bc37afe1e015b01b3f81a60241acbba8488f
Christian <christian.veelken@cern.ch>
2015-12-19 16:39:37 +0100

    added debug output (disabled by default)

--- src/HadTauTFCrystalBall.cc
+++ src/HadTauTFCrystalBall.cc
@@ -7,6 +7,7 @@
 #include <string>
 #include <assert.h> 
 #include <math.h>
+#include <cmath>
 
 namespace
 {
@@ -111,73 +112,93 @@ namespace
 {
   double fnc_dscb(double* xx, double* pp) 
   { 
-    double x   = xx[0];
+    //std::cout << "<fnc_dscb>:" << std::endl;
+    double x = xx[0];
     // gaussian core
-    //double N   = pp[0]; // norm
-    double mu  = pp[1]; // mean
+    //double N = pp[0]; // norm
+    double mu = pp[1]; // mean
+    if ( mu < 0.5 ) mu = 0.5;
+    if ( mu > 1.5 ) mu = 1.5;
     double sig = TMath::Max(1.e-2, pp[2]); // variance
     // transition parameters
-    double a1  = pp[3];
-    double p1  = pp[4];
-    double a2  = pp[5];
-    double p2  = pp[6];
+    double a1 = TMath::Max(0., pp[3]);
+    double p1 = TMath::Max(0., pp[4]);
+    if ( p1 > 1.e+1 ) p1 = 1.e+1;
+    double a2 = TMath::Max(0., pp[5]);
+    double p2 = TMath::Max(0., pp[6]);
+    if ( p2 > 1.e+1 ) p2 = 1.e+1;
+    //std::cout << "a1 = " << a1 << ", p1 = " << p1 << ", a2 = " << a2 << ", p2 = " << p2 << std::endl;
     //
-    double u   = (x - mu)/sig;
-    double A1  = pow(p1/fabs(a1), p1)*exp(-0.5*a1*a1);
-    double A2  = pow(p2/fabs(a2), p2)*exp(-0.5*a2*a2);
-    double B1  = p1/fabs(a1) - fabs(a1);
-    double B2  = p2/fabs(a2) - fabs(a2);
-    //
-    double a1_plus_B1 = a1 + B1;
-    std::cout << "a1 + B1 = " << a1_plus_B1 << ", mu + B1 = " << (mu + B1) << ", -1 + p1 = " << (-1. + p1) << std::endl;  
-    double term1 = A1*TMath::Power(a1_plus_B1, -p1)*(a1_plus_B1*sig - TMath::Power(a1_plus_B1*sig, p1)*TMath::Power(mu + B1*sig, 1. - p1))/((-1. + p1)*sig);
+    double u = (x - mu)/sig;
+    double A1 = 0.;
+    double B1 = 0.;
+    double term1 = 0.;
+    if ( a1 > 0. && p1 > 0. && p1 < 1.e+1 ) {
+      A1 = pow(p1/fabs(a1), p1)*exp(-0.5*a1*a1);
+      B1 = p1/fabs(a1) - fabs(a1);
+      double a1_plus_B1 = a1 + B1;
+      //std::cout << "A1 = " << A1 << ", a1 + B1 = " << a1_plus_B1 << ", mu + B1*sig = " << (mu + B1*sig) << ", -1 + p1 = " << (-1. + p1) << std::endl;
+      term1 = A1*TMath::Power(a1_plus_B1, -p1)*(a1_plus_B1*sig - TMath::Power(a1_plus_B1*sig, p1)*TMath::Power(TMath::Max(0., mu + B1*sig), 1. - p1))/((-1. + p1)*sig);
+      if ( !std::isfinite(term1) ) term1 = 0.;
+    }
     const double one_over_sqrtTwo = 1./TMath::Sqrt(2.);
-    const double sqrtPi_over_two = TMath::Sqrt(0.5*TMath::Pi());    
+    const double sqrtPi_over_two = TMath::Sqrt(0.5*TMath::Pi());
     double term2 = sqrtPi_over_two*(TMath::Erf(a1*one_over_sqrtTwo) + TMath::Erf(a2*one_over_sqrtTwo));
-    double a2_plus_B2 = a2 + B2;
-    double term3 = A2*TMath::Power(a2_plus_B2, -p2)*(a2_plus_B2*sig - TMath::Power(a2_plus_B2*sig, p2)*TMath::Power(2. - mu + B2*sig, 1. - p2))/((-1. + p2)*sig);
-    std::cout << "term1 = " << term1 << ", term2 = " << term2 << ", term3 = " << term3 << ", sig = " << sig << std::endl; 
+    double A2 = 0.;
+    double B2 = 0.;
+    double term3 = 0.;
+    if ( a2 > 0. && p2 > 0. && p2 < 1.e+1 ) {
+      A2 = pow(p2/fabs(a2), p2)*exp(-0.5*a2*a2);
+      B2 = p2/fabs(a2) - fabs(a2);
+      double a2_plus_B2 = a2 + B2;
+      //std::cout << "A2 = " << A2 << ", a2 + B2 = " << a2_plus_B2 << ", 2. - mu + B2*sig = " << (2. - mu + B2*sig) << ", -1 + p2 = " << (-1. + p2) << std::endl;
+      term3 = A2*TMath::Power(a2_plus_B2, -p2)*(a2_plus_B2*sig - TMath::Power(a2_plus_B2*sig, p2)*TMath::Power(TMath::Min(2., 2. - mu + B2*sig), 1. - p2))/((-1. + p2)*sig);
+      if ( !std::isfinite(term3) ) term3 = 0.;
+    } 
+    //std::cout << "term1 = " << term1 << ", term2 = " << term2 << ", term3 = " << term3 << ", sig = " << sig << std::endl; 
     double one_over_N = term1 + term2 + term3;
     one_over_N *= sig; // CV: multiply result obtained from Mathematica by Jacobi factor dx/du = sig for variable transformation from x to u
-    std::cout << "1/N = " << one_over_N << std::endl;
-    double N = 1./one_over_N;
+    //std::cout << "1/N = " << one_over_N << std::endl;
+    double N = ( one_over_N > 1.e-6 ) ? 1./one_over_N : 0.;
     //
     double result = 1.;
     if ( u < -a1 ) {
-      std::cout << "B1 - u = " <<  (B1 - u) << std::endl;
-      result = N*A1*pow(B1 - u, -p1);
+      //std::cout << "N = " << N << ", A1 = " << A1 << ", B1 - u = " <<  (B1 - u) << std::endl;
+      result = N*A1*TMath::Power(B1 - u, -p1);
     } else if ( u <  a2 ) {
-      std::cout << "u = " << u << std::endl;	
-      result = N*exp(-0.5*u*u);
+      double arg = -0.5*u*u;
+      //std::cout << "N = " << N << ", u = " << u << ", arg = " << arg << std::endl;	
+      result = N*TMath::Exp(arg);
     } else {
-      std::cout << "B1 - u = " <<  (B1 - u) << std::endl;
-      result = N*A2*pow(B2 + u, -p2);
+      //std::cout << "N = " << N << ", A2 = " << A2 << ", B2 + u = " <<  (B2 + u) << std::endl;
+      result = N*A2*TMath::Power(B2 + u, -p2);
     }
+    //std::cout << "--> result = " << result << std::endl;
     return result;
   }
 }
 
 double HadTauTFCrystalBall::operator()(double recPt, double genPt, double genEta) const
 { 
-  std::cout << "<HadTauTFCrystalBall::operator()>:" << std::endl;
-  std::cout << " pT: rec = " << recPt << ", gen = " << genPt << std::endl;
-  std::cout << " eta = " << genEta << std::endl;
+  //std::cout << "<HadTauTFCrystalBall::operator()>:" << std::endl;
+  //std::cout << " pT: rec = " << recPt << ", gen = " << genPt << std::endl;
+  //std::cout << " eta = " << genEta << std::endl;
 
   xx_[0] = recPt/genPt; 
-  std::cout << " xx = " << xx_[0] << std::endl;
+  //std::cout << " xx = " << xx_[0] << std::endl;
 
   if ( genPt != genPt_cache_ || genEta != genEta_cache_ ) {
     for ( int iPar = 1; iPar < cbParSize_; ++iPar ) {
       pp_[iPar] = (*thePar_[iPar])(genPt, genEta);
       pp_[iPar] = 1./(*thePar_[iPar])(genPt, genEta); // CV: temporary fix 
-      std::cout << " pp(" << iPar << ") = " << pp_[iPar] << std::endl;
+      //std::cout << " pp(" << iPar << ") = " << pp_[iPar] << std::endl;
     }
     genPt_cache_ = genPt;
     genEta_cache_ = genEta;
   }
 
   double retVal = fnc_dscb(xx_, pp_);
-  std::cout << "--> returning retVal = " << retVal << std::endl;	
+  //std::cout << "--> returning retVal = " << retVal << std::endl;	
   return retVal;
 }
 
